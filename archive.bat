472d1cab6d78f0863c40aa10db8f2ee975fee55f
Oleg Stepanov <stepanovoleg.dev@gmail.com>
2021-02-10 20:23:32 +0300

    Fix folder structure and archive process

--- archive.bat
+++ archive.bat
@@ -1,4 +1,1 @@
-"C:\Program Files\7-Zip\7z.exe" a -tzip ^
-modifiers_quick_actions.zip ^
-modules ^
-__init__.py
+"C:\Program Files\7-Zip\7z.exe" a -tzip modifiers_quick_actions.zip modifiers_quick_actions\
